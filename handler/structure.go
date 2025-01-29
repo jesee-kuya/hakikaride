b559bebdd1bfa33bac4bdc8a569b7ea4bfdba7d3
jkuya <kuyajesee@proton.me>
2025-01-29 17:17:46 +0300

    Feat: declare structs to pass to the front end

---
+++ handler/structure.go
@@ -0,0 +1,47 @@
+package Handler
+
+type Boarding struct {
+	Title string
+	Data  any
+}
+
+type Details struct {
+	School    string
+	BusDetail any
+	Students  []Student
+}
+
+type Student struct {
+	Name        string
+	Grade       string
+	School      string
+	ID          int
+	Route       BuuDetail
+	PickUpPoint string
+}
+
+type BuuDetail struct {
+	RouteName string
+	ID        string
+	Number    string
+	Driver    string
+}
+
+type Error struct {
+	StatusCode int
+	ErrMsg     string
+}
+
+type Parent struct {
+	ParentInitial string
+	ParentName    string
+	ParentEmail   string
+}
+
+type School struct {
+	Name          string
+	Email         string
+	TotalBuses    int
+	ActiveRoutes  []BuuDetail
+	TotalStudents []Student
+}
