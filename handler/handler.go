ba0cbd978fc0d6c66ef15e2f3d7702cbd2e0a558
jkuya <kuyajesee@proton.me>
2025-01-31 18:39:53 +0300

    Feat: define func HandleAuth

--- handler/handler.go
+++ handler/handler.go
@@ -21,7 +21,36 @@ func HandleHome(w http.ResponseWriter, r *http.Request) {
 	var data Data
 	if r.Method != http.MethodGet {
 		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
+		return
 	}
 	data.Title = "home"
 	Temp.ExecuteTemplate(w, "base.html", data)
+}
+
+func HandleAuth(w http.ResponseWriter, r *http.Request) {
+	var data Data
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not Allowed", http.StatusMethodNotAllowed)
+		return
+	} else {
+		data.Title = "auth"
+		Temp.ExecuteTemplate(w, "base.html", data)
+	}
+	// 	// Get form action (login or signup) and user type (school or parent)
+	// 	action := r.FormValue("action")     // "login" or "signup"
+	// 	userType := r.FormValue("userType") // "school" or "parent"
+
+	// 	if action == "" || userType == "" {
+	// 		HandleError(w, "Missing form action or user type", http.StatusBadRequest)
+	// 		return
+	// 	}
+
+	//		switch action {
+	//		case "login":
+	//			handleLogin(w, r, userType)
+	//		case "signup":
+	//			handleSignup(w, r, userType)
+	//		default:
+	//			HandleError(w, "Invalid action", http.StatusBadRequest)
+	//		}
 }
