bdd0e352e1da48d480c63691c1931b7e4157d1f2
jkuya <kuyajesee@proton.me>
2025-02-27 17:42:59 +0300

    feat: add error handling for 404 and unexpected errors in ParentHandler

--- backend/handlers/parentHandler.go
+++ backend/handlers/parentHandler.go
@@ -3,11 +3,22 @@ package handler
 import "net/http"
 
 func ParentHandler(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+	if r.URL.Path != "/parent" {
+		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
 		return
 	}
+
+	if r.Method != http.MethodGet {
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		return
+	}
+
 	var data Data
 	data.Title = "parent"
-	Tmpl.ExecuteTemplate(w, "base.html", data)
+
+	err := Tmpl.ExecuteTemplate(w, "base.html", data)
+	if err != nil {
+		ErrorHandler(w, "Unexpected Error Occured. Try again later", http.StatusInternalServerError)
+		return
+	}
 }
