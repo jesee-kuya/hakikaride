0e2ca1736a2cc4da988c696b14d892e09249ebff
jkuya <kuyajesee@proton.me>
2025-02-27 16:31:20 +0300

    feat: enhance ParentHandler to handle non-GET requests with appropriate error response

--- backend/handlers/parentHandler.go
+++ backend/handlers/parentHandler.go
@@ -3,10 +3,11 @@ package handler
 import "net/http"
 
 func ParentHandler(w http.ResponseWriter, r *http.Request) {
-	var data Data
-	if r.Method == http.MethodGet {
-		data.Title = "parent"
-		Tmpl.ExecuteTemplate(w, "base.html", data)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	var data Data
+	data.Title = "parent"
+	Tmpl.ExecuteTemplate(w, "base.html", data)
 }
