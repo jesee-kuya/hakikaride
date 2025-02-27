36ec7aeb53ea03d8ca322db1c6a7b7bc2acb43c6
jkuya <kuyajesee@proton.me>
2025-02-27 17:42:43 +0300

    feat: enhance IndexHandler with error handling for 404 and template execution

--- backend/handlers/indexHandler.go
+++ backend/handlers/indexHandler.go
@@ -13,11 +13,19 @@ type Data struct {
 var Tmpl, Err = template.ParseGlob("frontend/template/*.html")
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
+		return
+	}
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	var data Data
 	data.Title = "home"
-	Tmpl.ExecuteTemplate(w, "base.html", data)
+	err := Tmpl.ExecuteTemplate(w, "base.html", data)
+	if err != nil {
+		ErrorHandler(w, "Unexpected Error Occured. Try again later", http.StatusInternalServerError)
+		return
+	}
 }
