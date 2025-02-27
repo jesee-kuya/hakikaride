9a86243b3f0ff11c3ca9eec285e1e42c6d02237d
jkuya <kuyajesee@proton.me>
2025-02-27 17:41:58 +0300

    feat: add error handling for template execution in AuthHandler

--- backend/handlers/authHandler.go
+++ backend/handlers/authHandler.go
@@ -6,7 +6,10 @@ func AuthHandler(w http.ResponseWriter, r *http.Request) {
 	var data Data
 	if r.Method == http.MethodGet {
 		data.Title = "auth"
-		Tmpl.ExecuteTemplate(w, "base.html", data)
-		return
+		err := Tmpl.ExecuteTemplate(w, "base.html", data)
+		if err != nil {
+			ErrorHandler(w, "Unexpected Error Occured. Try again later", http.StatusInternalServerError)
+			return
+		}
 	}
 }
