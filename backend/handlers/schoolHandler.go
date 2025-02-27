df3d7f8ff40926cad873c27073bb9b1760d65aee
jkuya <kuyajesee@proton.me>
2025-02-27 16:34:27 +0300

    feat: update SchoolHandler to return 405 for non-GET requests

--- backend/handlers/schoolHandler.go
+++ backend/handlers/schoolHandler.go
@@ -3,10 +3,11 @@ package handler
 import "net/http"
 
 func SchoolHandler(w http.ResponseWriter, r *http.Request) {
-	var data Data
-	if r.Method == "GET" {
-		data.Title = "school"
-		Tmpl.ExecuteTemplate(w, "base.html", data)
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
+	var data Data
+	data.Title = "school"
+	Tmpl.ExecuteTemplate(w, "base.html", data)
 }
