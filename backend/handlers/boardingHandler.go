d13d97b92611942b9f14a089ee0cae5a2326a0b1
jkuya <kuyajesee@proton.me>
2025-02-27 17:42:07 +0300

    feat: enhance BoardingHandler to include 404 error handling and improve error responses

--- backend/handlers/boardingHandler.go
+++ backend/handlers/boardingHandler.go
@@ -3,11 +3,19 @@ package handler
 import "net/http"
 
 func BoardingHandler(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/boarding" {
+		ErrorHandler(w, "Page Not Found", http.StatusNotFound)
+		return
+	}
 	if r.Method != http.MethodGet {
-		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+		ErrorHandler(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 	var data Data
 	data.Title = "boarding"
-	Tmpl.ExecuteTemplate(w, "base.html", data)
+	err := Tmpl.ExecuteTemplate(w, "base.html", data)
+	if err != nil {
+		ErrorHandler(w, "Unexpected Error Occured. Try again later", http.StatusInternalServerError)
+		return
+	}
 }
